07172f7da68cfcca7d28e423139aae488ea0b7c1
vladpostu <vladposth@gmail.com>
2025-01-20 21:01:43 +0100

    release v2

--- js/js.js
+++ js/js.js
@@ -1,4 +1,8 @@
+let jackpot = 0;
+let current_reel = "";
+
 function spinBtnClick() {
+    jackpot = 0;
     let reelm2 = document.querySelector('.reels-m2');
     let reelm1 = document.querySelector('.reels-m1');
     let reel0 = document.querySelector('.reels-0');
@@ -6,8 +10,15 @@ function spinBtnClick() {
 
     let imgs = document.querySelectorAll('.reels-m2 .reel img')
     imgs.forEach((img) => {
-        img.src = "./imgs/reel-" + (Math.floor(Math.random() * 4) + 1) + ".png"
+        img.src = "./imgs/reel-" + (Math.floor(Math.random() * 3) + 1) + ".png"
+
+        if (img.src == "./imgs/reel-1.png") {
+            jackpot++;
+        }
+
     });
+
+
 
     reelm2.classList.add('reels-m1');
     reelm1.classList.add('reels-0');
@@ -20,10 +31,90 @@ function spinBtnClick() {
     reel1.classList.remove('reels-1');
 }
 
+
+function jackpot_trigger() {
+    let reel0 = document.querySelector('.reels-0');
+    let imgs = document.querySelectorAll('.reels-0 .reel img');
+    jackpot = 0;
+
+    imgs.forEach((img) => {
+        if (img.src.includes("reel-1.png")) {
+            jackpot++;
+        }
+    })
+
+    if (jackpot >= 3) {
+        document.querySelector("#jackpot_audio").play();
+        imgs.forEach((img) => {
+            if (img.src.includes("reel-1.png")) {
+                console.log("ok");
+                let video = document.createElement("video");
+                video.loop = true;
+                video.muted = true;
+                video.autoplay = true;
+
+                let source = document.createElement("source");
+                source.src = "/imgs/video.mp4";
+                source.type = "video/mp4";
+                video.appendChild(source);
+
+                img.replaceWith(video);
+            }
+        })
+
+    }
+}
+
+function reset() {
+    let videos = document.querySelectorAll('.reels-0 .reel video');
+    console.log(videos);
+
+
+    videos.forEach((video) => {
+        // Extract the source element from the video
+            // Create a new <img> element to replace the video
+            let img = document.createElement("img");
+            img.src = "./imgs/reel-1.png"; 
+            img.alt = "Reel Image";
+
+            video.replaceWith(img);
+    });
+}
+
+
 let spinBtn = document.querySelector('.spin-btn');
 spinBtn.addEventListener('click', () => {
+
+    spinBtn.disabled = true;
+
+    reset();
+
     spinBtnClick()
+    var audio = document.getElementById("spin_sound");
+    audio.play();
+
+    setTimeout(() => spinBtnClick(), 350);
+    setTimeout(() => spinBtnClick(), 700);
+    setTimeout(() => spinBtnClick(), 1050);
+    setTimeout(() => spinBtnClick(), 1400);
+    setTimeout(() => spinBtnClick(), 1750);
+    setTimeout(() => {
+        spinBtnClick();
+    }, 2100);
+
+    setTimeout(() => {
+        jackpot_trigger();
+        spinBtn.disabled = false;
+    }, 2500);
+
+    
+
 })
+
+
+let video = document.querySelector('#video');
+video.play();
+
 
 
 
